c973edb7694221aec1ef0307494995cc4eaccd20
Alain Tran <alaint77@gmail.com>
2019-07-25 23:26:07 +0200

    add react to client

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Navbar</h1>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Team</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
